2a5634465eb5208ef8e9fb1d0bb843c6916f6892
Mila Zolotova <l.zolotova@yahoo.com>
2022-09-25 02:33:28 +0300

    добавлена страница игры арканоид

--- assets/game_1/arcanoid.js
+++ assets/game_1/arcanoid.js
@@ -42,7 +42,7 @@ const game = {
     }
     for (const key in this.sprites) {
       this.sprites[key] = new Image();
-      this.sprites[key].src = `img/${key}.png`;
+      this.sprites[key].src = `../../assets/img/${key}.png`;
       this.sprites[key].addEventListener('load', imageLoad)
     }
   },
